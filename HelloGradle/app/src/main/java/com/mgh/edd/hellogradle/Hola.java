7cf6a4a9d56f907c64e9cfac93b281f1789069ed
claudia <clo.aguinaco.s@gmail.com>
2022-04-01 21:50:15 +0200

    Gradle mal

---
+++ HelloGradle/app/src/main/java/com/mgh/edd/hellogradle/Hola.java
@@ -0,0 +1,13 @@
+package com.mgh.edd.hellogradle;
+
+public class Hola {
+
+
+    public static void main(String[] args) {
+        String nombre=args.length>0?args[0]:"";
+        Saluda.saluda(nombre);
+
+    }
+}
+
+  
